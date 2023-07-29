922c69981be82431a99748dfdf53adad9bab0c08
owenthcarey <owenthcarey@gmail.com>
2023-07-29 00:15:55 -0700

    Update ffmpegframe.cpp

--- src/ffmpegframe.cpp
+++ src/ffmpegframe.cpp
@@ -17,10 +17,40 @@ FfmpegFrame::FfmpegFrame() : wxFrame(NULL, wxID_ANY,
     resolutionCtrl = new wxTextCtrl(this, wxID_ANY);
     bitrateCtrl = new wxTextCtrl(this, wxID_ANY);
     formatCtrl = new wxComboBox(this, wxID_ANY);
-    // Set layout and other components...
     outputDirectoryCtrl = new wxDirPickerCtrl(this, wxID_ANY);
     outputFileNameCtrl = new wxTextCtrl(this, wxID_ANY);
+
+    wxButton *compressButton = new wxButton(this, wxID_EXECUTE, "Compress");
+    wxButton *exitButton = new wxButton(this, wxID_EXIT, "Exit");
+
+    wxFlexGridSizer *gridSizer = new wxFlexGridSizer(2, 5, 5);
+    gridSizer->AddGrowableCol(1, 1);
+
+    gridSizer->Add(new wxStaticText(this, wxID_ANY, "Input File:"));
+    gridSizer->Add(filePickerCtrl, 1, wxEXPAND);
+
+    gridSizer->Add(new wxStaticText(this, wxID_ANY, "Resolution:"));
+    gridSizer->Add(resolutionCtrl, 1, wxEXPAND);
+
+    gridSizer->Add(new wxStaticText(this, wxID_ANY, "Bitrate:"));
+    gridSizer->Add(bitrateCtrl, 1, wxEXPAND);
+
+    gridSizer->Add(new wxStaticText(this, wxID_ANY, "Format:"));
+    gridSizer->Add(formatCtrl, 1, wxEXPAND);
+
+    gridSizer->Add(new wxStaticText(this, wxID_ANY, "Output Directory:"));
+    gridSizer->Add(outputDirectoryCtrl, 1, wxEXPAND);
+
+    gridSizer->Add(new wxStaticText(this, wxID_ANY, "Output File Name:"));
+    gridSizer->Add(outputFileNameCtrl, 1, wxEXPAND);
+
+    wxBoxSizer *boxSizer = new wxBoxSizer(wxVERTICAL);
+    boxSizer->Add(gridSizer, 1, wxALL | wxEXPAND, 15);
+    boxSizer->Add(compressButton, 0, wxALL | wxALIGN_CENTER, 15);
+    boxSizer->Add(exitButton, 0, wxALL | wxALIGN_CENTER, 15);
+    SetSizer(boxSizer);
 }
+
 
 void FfmpegFrame::OnCompress(wxCommandEvent &WXUNUSED(event)) {
     wxString input = filePickerCtrl->GetPath();
