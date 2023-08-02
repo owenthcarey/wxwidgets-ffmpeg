a7391d9a9c0df489edd6a2a740c2ca26bd4401b1
owenthcarey <owenthcarey@gmail.com>
2023-08-01 17:33:34 -0700

    Update ffmpegframe.cpp

--- src/ffmpegframe.cpp
+++ src/ffmpegframe.cpp
@@ -12,46 +12,51 @@ wxEND_EVENT_TABLE()
 FfmpegFrame::FfmpegFrame() : wxFrame(NULL, wxID_ANY,
                                      "wxWidgets FFmpeg Video Compressor",
                                      wxDefaultPosition, wxSize(800, 600)) {
+    // Input settings
+    wxFlexGridSizer *inputSettingsSizer = new wxFlexGridSizer(2, 5, 5);
+    inputSettingsSizer->AddGrowableCol(1, 1);
+
     filePickerCtrl = new wxFilePickerCtrl(this, wxID_ANY);
-    resolutionCtrl = new wxTextCtrl(this, wxID_ANY);
-    bitrateCtrl = new wxTextCtrl(this, wxID_ANY);
-    crfSlider = new wxSlider(this, wxID_ANY, 23, 0, 51);
-    formatCtrl = new wxComboBox(this, wxID_ANY);
+    inputSettingsSizer->Add(new wxStaticText(this, wxID_ANY, "Input File:"));
+    inputSettingsSizer->Add(filePickerCtrl, 1, wxEXPAND);
+
+    // Output settings
+    wxStaticBox* outputSettingsBox = new wxStaticBox(this, wxID_ANY, "Output Settings");
+    wxStaticBoxSizer* outputSettingsSizer = new wxStaticBoxSizer(outputSettingsBox, wxVERTICAL);
+
     outputDirectoryCtrl = new wxDirPickerCtrl(this, wxID_ANY);
+    outputSettingsSizer->Add(new wxStaticText(this, wxID_ANY, "Output Directory:"));
+    outputSettingsSizer->Add(outputDirectoryCtrl, 0, wxALL | wxEXPAND, 5);
+
     outputFileNameCtrl = new wxTextCtrl(this, wxID_ANY);
+    outputSettingsSizer->Add(new wxStaticText(this, wxID_ANY, "Output File Name:"));
+    outputSettingsSizer->Add(outputFileNameCtrl, 0, wxALL | wxEXPAND, 5);
 
+    resolutionCtrl = new wxTextCtrl(this, wxID_ANY);
+    outputSettingsSizer->Add(new wxStaticText(this, wxID_ANY, "Video Resolution:"), 0, wxALL, 5);
+    outputSettingsSizer->Add(resolutionCtrl, 0, wxALL | wxEXPAND, 5);
+
+    bitrateCtrl = new wxTextCtrl(this, wxID_ANY);
+    outputSettingsSizer->Add(new wxStaticText(this, wxID_ANY, "Video Bitrate:"), 0, wxALL, 5);
+    outputSettingsSizer->Add(bitrateCtrl, 0, wxALL | wxEXPAND, 5);
+
+    crfSlider = new wxSlider(this, wxID_ANY, 23, 0, 51);
+    outputSettingsSizer->Add(new wxStaticText(this, wxID_ANY, "Video CRF:"), 0, wxALL, 5);
+    outputSettingsSizer->Add(crfSlider, 0, wxALL | wxEXPAND, 5);
+
+    formatCtrl = new wxComboBox(this, wxID_ANY);
+    outputSettingsSizer->Add(new wxStaticText(this, wxID_ANY, "Video Format:"), 0, wxALL, 5);
+    outputSettingsSizer->Add(formatCtrl, 0, wxALL | wxEXPAND, 5);
+
+    // Group all sizers together
+    wxBoxSizer *mainSizer = new wxBoxSizer(wxVERTICAL);
+    mainSizer->Add(inputSettingsSizer, 1, wxALL | wxEXPAND, 15);
+    mainSizer->Add(outputSettingsSizer, 1, wxALL | wxEXPAND, 15);
     wxButton *compressButton = new wxButton(this, wxID_EXECUTE, "Compress");
+    mainSizer->Add(compressButton, 0, wxALL | wxALIGN_CENTER, 15);
     wxButton *exitButton = new wxButton(this, wxID_EXIT, "Exit");
-
-    wxFlexGridSizer *gridSizer = new wxFlexGridSizer(2, 5, 5);
-    gridSizer->AddGrowableCol(1, 1);
-
-    gridSizer->Add(new wxStaticText(this, wxID_ANY, "Input File:"));
-    gridSizer->Add(filePickerCtrl, 1, wxEXPAND);
-
-    gridSizer->Add(new wxStaticText(this, wxID_ANY, "Resolution:"));
-    gridSizer->Add(resolutionCtrl, 1, wxEXPAND);
-
-    gridSizer->Add(new wxStaticText(this, wxID_ANY, "Bitrate:"));
-    gridSizer->Add(bitrateCtrl, 1, wxEXPAND);
-
-    gridSizer->Add(new wxStaticText(this, wxID_ANY, "CRF:"));
-    gridSizer->Add(crfSlider, 1, wxEXPAND);
-
-    gridSizer->Add(new wxStaticText(this, wxID_ANY, "Format:"));
-    gridSizer->Add(formatCtrl, 1, wxEXPAND);
-
-    gridSizer->Add(new wxStaticText(this, wxID_ANY, "Output Directory:"));
-    gridSizer->Add(outputDirectoryCtrl, 1, wxEXPAND);
-
-    gridSizer->Add(new wxStaticText(this, wxID_ANY, "Output File Name:"));
-    gridSizer->Add(outputFileNameCtrl, 1, wxEXPAND);
-
-    wxBoxSizer *boxSizer = new wxBoxSizer(wxVERTICAL);
-    boxSizer->Add(gridSizer, 1, wxALL | wxEXPAND, 15);
-    boxSizer->Add(compressButton, 0, wxALL | wxALIGN_CENTER, 15);
-    boxSizer->Add(exitButton, 0, wxALL | wxALIGN_CENTER, 15);
-    SetSizer(boxSizer);
+    mainSizer->Add(exitButton, 0, wxALL | wxALIGN_CENTER, 15);
+    SetSizer(mainSizer);
 }
 
 void FfmpegFrame::OnCompress(wxCommandEvent &WXUNUSED(event)) {
